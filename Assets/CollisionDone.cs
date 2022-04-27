e2471e7f98ebb45c2fd63b2037f5029ee4d70d92
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-04-27 17:34:01 +0530

    Joints are added.

---
+++ Assets/CollisionDone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter State");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision Exit State");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision Stay State");
+    }
+}
+
